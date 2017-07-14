eaf8e1c46c50a947f01e63be9b89ed62b8c752a9
Emanuel Suriano <emanuel.suriano@gmail.com>
2017-07-14 20:46:29 -0300

    Update Letters.js

--- js/components/Letters.js
+++ js/components/Letters.js
@@ -1,18 +1,34 @@
 import React, { Component, PropTypes } from 'react';
-import { Point, Container } from '../sharedStyles';
+import { Point, Container, LeftSide, RightSide } from '../sharedStyles';
+import brailleGenerator from '../utils/brailleGenerator';
+
+const Letter = ({ points }) => (
+  <div>
+    <LeftSide>
+      <Point enable={points[0]} />
+      <Point enable={points[1]} />
+      <Point enable={points[2]} />
+    </LeftSide>
+    <RightSide>
+      <Point enable={points[3]} />
+      <Point enable={points[4]} />
+      <Point enable={points[5]} />
+    </RightSide>
+  </div>
+);
 
 export default class Letters extends Component {
   static propTypes = {
     word: React.PropTypes.string,
   };
 
-  convertWordToPoints = () =>
-
   render() {
+    const brailleText = brailleGenerator(this.props.word);
     return (
       <Container>
-        <Point />
+        { brailleText.map(points => <Letter points={points} />)}
       </Container>
     );
   }
 };
+
